c28b8b406462f3c8edab95f424a608fd7877feb9
Delfan <faceninja95@gmail.comm>
2025-02-15 21:49:35 +0700

    added styles.css and death star background.

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import CharacterList from './components/CharacterList';
 import CharacterDetail from './components/CharacterDetail';
+import './styles.css';
 
 const App: React.FC = () => {
 	return (
